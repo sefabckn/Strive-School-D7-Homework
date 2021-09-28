f3e7f99d9c06d6f7290b8ee310069381f8808f56
sefabckn <sefabckn@gmail.com>
2021-09-28 16:04:38 +0200

    ex1 solved
    SecreenShot will be uploaded

--- D7-Homework.js
+++ D7-Homework.js
@@ -1,10 +1,5 @@
 /*
-
- 
-
 These additional exercises, from 7 to 12, should be written in JavaScript:
-
- 
 
 EX7: Write a function to change the h1 text to something else
 
@@ -17,8 +12,9 @@ EX10: Write a function to add a CSS class to every Amazon link
 EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
 
 EX12: Write a function to color the price of each product in a different one every time it's invoked*/
-
+//Those codes work on Console of webpage
 const changingHeaderTag1 = function(){
-    document.getElementById("Main_Header").innerHTML = "The Best Market in The World";
+    let headerChanging= document.getElementById("Main_Header").innerHTML = "The Best Market in The World";
+    return headerChanging
 }
 changingHeaderTag1()
